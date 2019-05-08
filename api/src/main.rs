8f067ad3b8819e98e0a76f659340d75d88153b00
Will Squire <will_squire@hotmail.co.uk>
2019-05-08 11:52:32 +0100

    Extracted bin & lib entrypoints as inte. test prep

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,56 +1,10 @@
-extern crate argon2;
-extern crate chrono;
-#[macro_use]
-extern crate diesel;
-extern crate jsonwebtoken as jwt;
-#[macro_use]
-extern crate juniper;
-extern crate clap;
-extern crate juniper_warp;
-extern crate r2d2;
-#[macro_use]
-extern crate serde;
-extern crate uuid;
-extern crate warp;
+extern crate api;
 
-mod config;
-mod context;
-mod db;
-mod error;
-mod hasher;
-mod models;
-mod routes;
-mod tokeniser;
+use api::{config::Config, error::Error, run};
 
-use config::Config;
-use db::Db;
-use error::Error;
-use hasher::Hasher;
-use routes::graphql::{context, graphql};
-use std::sync::Arc;
-use tokeniser::Tokeniser;
-use warp::Filter;
-
+/// Entry point for binary only.
+/// Use `lib.rs` for testing.
 fn main() -> Result<(), Error> {
   let config = Config::new()?;
-  let db = Arc::new(Db::new(
-    &config.db_user,
-    &config.db_password,
-    &config.db_name,
-    &config.db_server,
-  )?);
-  let hasher = Arc::new(Hasher::new(config.hash_salt));
-  let tokeniser = Arc::new(Tokeniser::new(config.token_secret));
-
-  let log = warp::log("warp_server");
-
-  warp::serve(
-    warp::get2()
-      .and(warp::path::end().and(juniper_warp::graphiql_filter("/graphql")))
-      .or(warp::path("graphql").and(graphql(context(db, hasher, tokeniser))))
-      .with(log),
-  )
-  .run(([127, 0, 0, 1], 8000));
-
-  Ok(())
+  run(config)
 }
